dd581596e22bc05c911c0c1a2561c514daca8464
aryaadg <aryaadiguna12@gmail.com>
2021-03-29 14:00:03 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ export default function IndexPage() {
     <div>
   
   <nav className="uk-navbar-container" uk-navbar="true">
+    <div className="uk-navbar-left">
 
         <ul className="uk-navbar-nav">
             <li className="uk-active"><a href="#">Active</a></li>
