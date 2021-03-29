45f2988fca455c79bb5ffd67ce1288b70cbe49df
aryaadg <aryaadiguna12@gmail.com>
2021-03-29 13:59:18 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
 export default function IndexPage() {
 	return (
     <div>
-      <p>soosososodsffdgdgtrrttso</p>
+  
+  <nav className="uk-navbar-container" uk-navbar="true">
+
+        <ul className="uk-navbar-nav">
+            <li className="uk-active"><a href="#">Active</a></li>
+            <li>
+                <a href="#">Parent</a>
+                <div className="uk-navbar-dropdown">
+                    <ul className="uk-nav uk-navbar-dropdown-nav">
+                        <li className="uk-active"><a href="#">Active</a></li>
+                        <li><a href="#">Item</a></li>
+                        <li><a href="#">Item</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="#">Item</a></li>
+        </ul>
+
+    </div>
+</nav>
+
     </div>
 	);
 }
